0648cb7cf3081f7bb08c947ecdd9196574a7b458
IAN AT HOME <Iannorton01@gmail.com>
2017-02-20 00:43:51 -0500

    New branch to impliment controller support. Rok support in. needs balancing,

--- Prototype Vitaliy/RescueValhalla_test/Assets/DwaneController.cs
+++ Prototype Vitaliy/RescueValhalla_test/Assets/DwaneController.cs
@@ -1,47 +1,54 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Rewired;
+using UnityEngine;
 
-public class DwaneController : MonoBehaviour {
+public class DwaneController: MonoBehaviour {
 
     [SerializeField] float m_moveSpeed = 3;
     [SerializeField] float m_spinSpeed = 30.0f;
     [SerializeField] float m_effectRadius = 2;
     [SerializeField] float m_pullForce = 2;
     [SerializeField] float m_pushForce = 5;
+    [SerializeField] float m_reactionaryForce = 5;
     private Rigidbody2D m_rb;
 
-    void Start () {
+
+    //rewired
+    public int playerId = 0;
+    private Player player; // The Rewired Player
+
+
+    void Awake ()
+    {
+        player = ReInput.players.GetPlayer(playerId); //Initializes the ReWired inputs  
+    }
+
+    void Start ()
+    {
+        player = ReInput.players.GetPlayer(playerId); //Initializes the ReWired inputs
         m_rb = GetComponent<Rigidbody2D>();
 	}
 	
-	void FixedUpdate()
+	void FixedUpdate ()
     {
-        if(Input.GetKey(KeyCode.A))
+        if (player.GetAxis("LHorizontal") != 0)
         {
-            MoveLeft();
+            MoveRock();
         }
-        if (Input.GetKey(KeyCode.D))
-        {
-            MoveRight();
-        }
-        if (Input.GetKey(KeyCode.S))
+        if (player.GetButton("AButton"))
         {
             PullEffect();
         }
-        if(Input.GetKeyDown(KeyCode.W))
+        if (player.GetButtonDown("BButton"))
         {
             PushEffect();
         }
     }
 
-    void MoveLeft()
+    void MoveRock()
     {
-        m_rb.AddTorque(m_spinSpeed);
-    }
-
-    void MoveRight()
-    {
-        m_rb.AddTorque(-m_spinSpeed);
+        m_rb.AddTorque(m_spinSpeed* player.GetAxis("LHorizontal"));
     }
 
     //void HandleInput()
@@ -57,14 +64,14 @@ public class DwaneController : MonoBehaviour {
         {
             if (item.GetComponent<Rigidbody2D>() && item.CompareTag("Interactive"))
             {
-                item.attachedRigidbody.AddForce((item.transform.position - transform.position).normalized * -m_pullForce, ForceMode2D.Impulse); // messy but if the detected collider has a rigidbody and is tagged as interactive a pull force is applied
-                m_rb.AddForce((item.transform.position - transform.position).normalized * m_pullForce, ForceMode2D.Force);
+                item.attachedRigidbody.AddForce((item.transform.position - transform.position).normalized * -m_pullForce, ForceMode2D.Force); // messy but if the detected collider has a rigidbody and is tagged as interactive a pull force is applied
+                m_rb.AddForce((item.transform.position - transform.position).normalized * m_pullForce , ForceMode2D.Force);
             }
 
             if (item.GetComponent<Rigidbody2D>() && item.CompareTag("FixedInteractive"))
             {
 
-                m_rb.AddForce((item.transform.position - transform.position).normalized * (m_pullForce * 3), ForceMode2D.Force);
+                m_rb.AddForce((item.transform.position - transform.position).normalized * m_pullForce * m_reactionaryForce, ForceMode2D.Force);
             }
         }
     }
@@ -83,7 +90,7 @@ public class DwaneController : MonoBehaviour {
             if (item.GetComponent<Rigidbody2D>() && item.CompareTag("FixedInteractive"))
             {
 
-                m_rb.AddForce((item.transform.position - transform.position).normalized * -m_pushForce, ForceMode2D.Impulse);
+                m_rb.AddForce((item.transform.position - transform.position).normalized * -m_pushForce * m_reactionaryForce, ForceMode2D.Impulse);
             }
         }
     }
