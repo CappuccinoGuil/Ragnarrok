cdbf6591c982dd6351cb1cde244c3dbdfb1f329e
IAN AT HOME <Iannorton01@gmail.com>
2017-02-21 01:15:02 -0500

    Controller support for Torn implimented

--- Prototype Vitaliy/RescueValhalla_test/Assets/playerControllerScript.cs
+++ Prototype Vitaliy/RescueValhalla_test/Assets/playerControllerScript.cs
@@ -1,66 +1,84 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Rewired;
 
 public class playerControllerScript : MonoBehaviour {
 
-	bool grab = false;
-	RaycastHit2D hit;
-	float distance = 0.7f;
-	public Transform holdpoint;
-	public float throwforce = 2f;
-
-	// Use this for initialization
-	void Start () {
 	
-	}
+	[SerializeField] float grabDistance = 0.7f; 
+    [SerializeField] Transform holdPoint;
+	[SerializeField] float throwforce = 2f;
+    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float jumpForce = 300f;
+    [SerializeField] float canJumpHeight = 0.15f;
 
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.RightArrow)){
-			transform.position = new Vector3(transform.position.x + 0.1f,transform.position.y,0f);
+    private bool grab = false;
+    private RaycastHit2D hit;
+
+    //rewired
+    public int playerId = 0;
+    private Player player; // The Rewired Player
+
+
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerId);//Initializes the ReWired inputs
+    }
+
+	void Update ()
+    
+     {
+		if (player.GetAxis("LHorizontal") > 0)
+        {
+            Debug.Log(player.GetAxisRaw("LHorizontal"));
+			transform.position = new Vector3(transform.position.x + moveSpeed,transform.position.y,0f);
 			transform.localScale = new Vector3 (-0.6f, transform.localScale.y, transform.localScale.z);
-           // transform.localRotation = new Quaternion()
         }
-        else if(Input.GetKey(KeyCode.LeftArrow)){
-			transform.position = new Vector3(transform.position.x - 0.1f,transform.position.y,0f);
+        if(player.GetAxis("LHorizontal") < 0)
+        {
+			transform.position = new Vector3(transform.position.x - moveSpeed,transform.position.y,0f);
 			transform.localScale = new Vector3 (0.6f, transform.localScale.y, transform.localScale.z);
 		}
 
-		if(Input.GetKeyDown(KeyCode.UpArrow)){
-			GetComponent<Rigidbody2D> ().AddForce(Vector2.up * 300f);
+		if(player.GetButtonDown("AButton") && GroundCheck()) //performs ground check, if player is withing range of ground they can jump
+        {
+			GetComponent<Rigidbody2D> ().AddForce(Vector2.up * jumpForce);
 		}
 
 		//Grabbing and Throwing
-		if(Input.GetKeyDown(KeyCode.M)){
-			if (!grab) {
+		if(player.GetButtonDown("BButton"))
+        {
+			if (!grab) 
+            {
 				print ("grab");
-				Physics2D.queriesStartInColliders = false;
-				hit = Physics2D.Raycast (transform.position, Vector2.left * transform.localScale.x, distance);
+				hit = Physics2D.CircleCast(transform.position, 1, Vector2.left * transform.localScale.x, grabDistance);
 				print (transform.localScale.x);
 
 				if (hit.collider != null && (hit.collider.gameObject.CompareTag("WoodenObject") || hit.collider.gameObject.CompareTag("PhysicsObject")|| hit.collider.gameObject.CompareTag("Dwane"))) {
 					print ("found");
 					grab = true;
-
 				}
-			} else if (grab) {
+			} 
+            else if (grab) 
+            {
 				print ("throw");
 				grab = false;
-				if (hit.collider.gameObject.GetComponent<Rigidbody2D> () != null) {
+				if (hit.collider.gameObject.GetComponent<Rigidbody2D> ()) 
+                {
 					hit.collider.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x*-2, 1) * throwforce;
-					//hit.collider.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (transform.localScale.x*-2, 1) * throwforce, ForceMode2D.Impulse);
 				}
 			}
 		}
 
 		if (grab) {
-			//print ("hold");
-			hit.collider.gameObject.transform.position = holdpoint.position;
+			hit.collider.gameObject.transform.position = holdPoint.position;
 		}
 	}
 
-	void OnDrawGizmos(){
-		Gizmos.color = Color.white;
-		Gizmos.DrawLine (transform.position, transform.position + Vector3.left *distance);
-	}
+    bool GroundCheck()
+    {
+        Debug.DrawRay(transform.position, -transform.up * canJumpHeight, Color.cyan, 2);
+        return Physics2D.Raycast(transform.position, -transform.up, canJumpHeight); 
+    }
+
 }
