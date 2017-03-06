8fcbfeb4b47c62ddbe53ed9c2992d09dc430e6fb
IAN AT HOME <Iannorton01@gmail.com>
2017-03-06 00:22:42 -0500

    added a new Ian_test scene to include the ragnar character in the level design fun. added ragnar png and added vertical functionality to the booster objects.

--- Prototype Vitaliy/RescueValhalla_test/Assets/_Scripts/Booster.cs
+++ Prototype Vitaliy/RescueValhalla_test/Assets/_Scripts/Booster.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Booster : MonoBehaviour {
     [SerializeField] float boostForce = 100000f;
+    [SerializeField] bool vertical = false;
+
+    private Vector2 direction = Vector2.right;
 
 	// Use this for initialization
 	void Start () {
@@ -12,10 +15,17 @@ public class Booster : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+	if (vertical)
+    {
+            direction = Vector2.up;
+
+    }
+    else { direction = Vector2.right; }
+
+
 	}
     void OnTriggerStay2D(Collider2D thingToBoost)
     {
-        thingToBoost.GetComponent<Rigidbody2D>().AddForce(Vector2.left * boostForce, ForceMode2D.Force);
+        thingToBoost.GetComponent<Rigidbody2D>().AddForce( direction * boostForce, ForceMode2D.Force);
     }
 }
