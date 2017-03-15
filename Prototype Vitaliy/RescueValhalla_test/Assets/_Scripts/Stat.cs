2c4362232bb5941798143e9c1313731490bb8e99
Mackenzie Wilson <mackenzie.w115@gmail.com>
2017-03-14 20:25:42 -0400

    Made a cool steam UI bar. Also made a subtract and add steam zones.

---
+++ Prototype Vitaliy/RescueValhalla_test/Assets/_Scripts/Stat.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+
+public class Stat
+{
+    [SerializeField]
+    private UIBar bar;
+
+    [SerializeField]
+    private float maxVal;
+
+    [SerializeField]
+    private float currentVal;
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            maxVal = value;
+            bar.fillMax = maxVal;
+        }
+    }
+
+    public float CurrentVal
+    {
+        get
+        {
+            return currentVal;
+        }
+
+        set
+        {
+            currentVal = Mathf.Clamp(value, 0, MaxVal);
+            bar.Value = currentVal;
+        }
+    }
+
+    public void Initialize()
+    {
+        MaxVal = maxVal;
+        CurrentVal = currentVal;
+    }
+
+}
