5fceb353b18b30848dbe165ed7dca1ec95e94d1d
Joshua Randall <josh.randall16@gmail.com>
2017-03-21 23:39:02 -0400

    Created Main Menu Scene
    -Created a main Menu Scene
    -Created a Second Scene
    -Added 3 buttons
    -Added Scene Transition Script
    -“Play Game” button works and goes to main game scene

---
+++ Prototype Vitaliy/RescueValhalla_test/Assets/_Scripts/UIScripts/SceneTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SceneTransition : MonoBehaviour {
+
+
+
+
+	//Attach this script to any button and just type in the scene name as a string.
+	public void NextLevelButton(string LevelName)
+	{
+		
+		Application.LoadLevel (LevelName);
+	}
+
+
+}
