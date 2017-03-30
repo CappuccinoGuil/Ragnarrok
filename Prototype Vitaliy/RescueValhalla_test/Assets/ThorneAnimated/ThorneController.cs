ba4ba53ddfdafa18006e46191ea47dbaf6a9bf4c
Joshua Randall <josh.randall16@gmail.com>
2017-03-30 00:57:22 -0400

    pull Master
    -pulled master

---
+++ Prototype Vitaliy/RescueValhalla_test/Assets/ThorneAnimated/ThorneController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThorneController : MonoBehaviour {
+
+	//movment variaables
+	public float maxSpeed;
+	Rigidbody2D myRB;
+	public Animator myAnim;
+	bool facingRight;
+	bool itemHeld = false;
+
+	//jumping variables
+
+	bool grounded = false;
+	float groundCheckRadius = 0.2f;
+	public LayerMask groundLayer;
+	public Transform groundCheck;
+	public float jumpHeight;
+
+	// Use this for initialization
+	void Start () {
+		facingRight = true;
+		myRB = GetComponent<Rigidbody2D> ();
+		//myAnim = GetComponentsInChildren<Animator> ();
+		print (myAnim);
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		if (grounded && Input.GetAxis ("Jump") > 0) {
+			grounded = false;
+			myAnim.SetBool ("isGrounded", grounded);
+			myRB.AddForce (new Vector2 (0, jumpHeight));
+		}
+		if (itemHeld) {
+
+			if (Input.GetButtonDown("Fire1")) {
+				myAnim.SetBool ("pickUp",false);
+				myAnim.SetBool ("isCharging",true);
+				//myAnim.SetTrigger ("isThrowing");
+			} else if (Input.GetButtonUp("Fire1")){
+				myAnim.SetBool ("isCharging",false);
+				myAnim.SetTrigger ("isThrowing");
+				itemHeld = false;
+			}
+
+		}else if (!itemHeld) {
+
+			if (Input.GetButtonDown ("Fire1")) {
+				myAnim.SetBool ("pickUp", true);
+				myAnim.SetBool ("isCharging", false);
+
+			} else if (Input.GetButtonUp ("Fire1")) {
+				itemHeld = true;
+			}
+
+		}
+	}
+
+
+	void FixedUpdate () {
+
+		//check if we are grounded
+		grounded = Physics2D.OverlapCircle(groundCheck.position,groundCheckRadius,groundLayer);
+		myAnim.SetBool ("isGrounded", grounded);
+		myAnim.SetFloat ("verticalSpeed", myRB.velocity.y);
+
+		float move = Input.GetAxis ("Horizontal");
+		myRB.velocity = new Vector2 (move * maxSpeed, myRB.velocity.y);
+		myAnim.SetFloat ("speed", Mathf.Abs (move));
+
+		if (move > 0 && !facingRight) {
+			Flip ();
+		} else if(move<0 &&facingRight){
+			Flip ();
+		}
+	}
+
+	void Flip (){
+
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
---
+++ Prototype Vitaliy/RescueValhalla_test/Assets/_Scripts/PlayerScripts/RagnarThrow.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+
+public class RagnarThrow : MonoBehaviour
+{
+
+    [SerializeField] float grabDistance = 1f;
+    [SerializeField] Transform holdPoint;
+    [SerializeField] GameObject m_pointer;
+    [SerializeField] float m_throwDistance = 1.0f;
+    [SerializeField] float m_rateOfThrowDistIncrease = 2.0f;
+
+    public bool m_isGrabbing = false;
+
+    private bool m_isThrowing = false;
+    private bool m_createAimer = false;
+    private bool m_isThereAnAimer = false;
+    private bool m_putDown = false;
+    private bool m_cancelThrow = false;
+
+    private float m_yVelocity;
+    private float m_xVelocity;
+    private float m_tempThrowDist;
+    private float m_axisActiveTime;
+
+    private RaycastHit2D hit;
+    private Quaternion m_tempHoldRotation;
+    public List<GameObject> createdAim;
+
+    Rigidbody2D m_rbHit;
+
+    playerControllerScript ragnar;
+
+    //rewired
+    public int playerId = 0;
+    private Player player; // The Rewired Player
+
+
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerId);//Initializes the ReWired inputs
+        m_tempThrowDist = m_throwDistance;
+        m_tempHoldRotation = holdPoint.rotation;
+    }
+
+    void Start()
+    {
+        ragnar = gameObject.GetComponent<playerControllerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (player.GetButtonDown("RTrigger") && !m_isGrabbing)
+        {
+            print("grab");
+            hit = Physics2D.CircleCast(transform.position, 0.25f, Vector2.right * transform.localScale.x, grabDistance);
+            m_rbHit = hit.rigidbody;
+
+            if (hit && (hit.collider.CompareTag("WoodenObject") || hit.collider.CompareTag("PhysicsObject") || hit.collider.CompareTag("Dwane")))
+            {
+                ragnar.myAnim.SetBool("pickUp", true);
+
+                print("found");
+                m_isGrabbing = true;
+                hit.collider.enabled = false;
+            }
+
+        }
+        else if (player.GetButtonDown("RTrigger") && m_isGrabbing && !m_isThereAnAimer)
+        {
+            if (ragnar.m_facingRight)
+            {
+                m_yVelocity = 2.5f;
+                m_xVelocity = 2.5f;
+            }
+            if (!ragnar.m_facingRight)
+            {
+                m_yVelocity = 2.5f;
+                m_xVelocity = 2.5f * -1;
+            }
+
+            m_isThrowing = true;
+            ragnar.myAnim.SetTrigger("isThrowing");
+        }
+        else if (player.GetButtonDown("RTrigger") && m_isThereAnAimer)
+        {
+            m_cancelThrow = true;
+            holdPoint.transform.rotation = m_tempHoldRotation;
+
+            ragnar.myAnim.SetBool("isCharging", false);
+            ragnar.myAnim.SetBool("pickUp", true);
+
+            Destroy(createdAim[0]);
+            createdAim.Clear();
+            ragnar.m_throwMode = false;
+            m_tempThrowDist = m_throwDistance;
+
+            m_isThereAnAimer = false;
+
+        }
+
+        if ((player.GetAxisRawTimeInactive("RHorizontal") > 0.1f || player.GetAxisRawTimeInactive("RVertical") > 0.1f))
+        {
+            m_cancelThrow = false;
+        }
+
+        if (!m_cancelThrow)
+        {
+            if (player.GetAxis("RHorizontal") > 0)
+            {
+                if (ragnar.m_facingRight)
+                {
+                    ragnar.Flip();
+                }
+            }
+            if (player.GetAxis("RHorizontal") < 0)
+            {
+                if (!ragnar.m_facingRight)
+                {
+                    ragnar.Flip();
+                }
+            }
+            if ((player.GetAxisRaw("RHorizontal") != 0 || player.GetAxisRaw("RVertical") != 0) && m_isGrabbing)
+            {
+                m_tempThrowDist += Time.deltaTime * m_rateOfThrowDistIncrease;
+            }
+            if ((player.GetAxisRaw("RHorizontal") != 0.0f || player.GetAxisRaw("RVertical") != 0.0f) && (m_isGrabbing && !m_isThereAnAimer))
+            {
+                ragnar.m_throwMode = true;
+                m_createAimer = true;
+            }
+            else if ((player.GetAxisRaw("RHorizontal") == 0 && player.GetAxisRaw("RVertical") == 0) && m_isThereAnAimer)
+            {
+                m_isThrowing = true;
+                holdPoint.transform.rotation = m_tempHoldRotation;
+
+                ragnar.myAnim.SetTrigger("isThrowing");
+
+                Destroy(createdAim[0]);
+                createdAim.Clear();
+                ragnar.m_throwMode = false;
+                m_tempThrowDist = m_throwDistance;
+
+                m_isThereAnAimer = false;
+            }
+        }
+
+        if (m_isGrabbing)
+        {
+            hit.transform.position = holdPoint.position;
+        }
+
+        if (m_createAimer)
+        {
+            ragnar.myAnim.SetBool("pickUp", false);
+            ragnar.myAnim.SetBool("isCharging", true);
+            CreateAimer();
+            m_isThereAnAimer = true;
+            m_createAimer = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (ragnar.m_throwMode && m_isThereAnAimer)
+        {
+            RotateAimer();
+            HandleThrow();
+            
+        }
+        if (m_isThrowing)
+        {
+            ragnar.myAnim.SetBool("pickUp", false);
+            ragnar.myAnim.SetBool("isCharging", false);
+
+            m_rbHit.velocity = new Vector2(m_xVelocity, m_yVelocity);
+            hit.collider.enabled = true;
+            m_isThrowing = false;
+            m_isGrabbing = false;
+        }
+    }
+
+    void HandleThrow()
+    {
+        int finalVelocity = 0;
+        Vector3 throwDirection = createdAim[0].transform.rotation.eulerAngles;
+        float theta = throwDirection.z * Mathf.Deg2Rad;
+        float initalVelocity = Mathf.Sqrt(finalVelocity - (2 * Physics2D.gravity.y * m_tempThrowDist));
+
+        m_yVelocity = (initalVelocity * Mathf.Cos(theta));
+        m_xVelocity = (initalVelocity * Mathf.Sin(theta)) * -1;
+
+    }
+
+    void RotateAimer()
+    {
+        float horz = player.GetAxisRaw("RHorizontal");
+        float vert = player.GetAxisRaw("RVertical");
+        float tarAngle = Mathf.Atan2(vert, horz) * Mathf.Rad2Deg;
+
+        createdAim[0].transform.rotation = Quaternion.Euler(0, 0, tarAngle + 90);
+        createdAim[0].transform.position = holdPoint.position + transform.forward * -0.5f;
+    }
+
+    void CreateAimer()
+    {
+        GameObject createdAimer;
+        createdAimer = Instantiate(m_pointer, holdPoint.position + transform.forward * -0.5f, holdPoint.rotation);
+        createdAim.Add(createdAimer);
+    }
+
+}
