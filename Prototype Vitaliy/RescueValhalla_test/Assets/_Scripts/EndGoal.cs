c3c529f266bd4930ce35883d9f8fd13b83061e35
IAN AT HOME <Iannorton01@gmail.com>
2017-04-10 23:09:38 -0400

    Made a Build scenes folder Re-named a bunch of the scenes:  JoshScene1 --> Menu,    asset_setup_scene  --> Level_01,   and    level_1 -->  Level_02 prefabbed the Re_wired Editor -----*****------ use the prefabbed version for new scenes from here on out please! Added end goal for players to reach, when both players are there it loads the next scene.- we can add in an animation of some sort if we need to and delay the scene load. Added transition from start menu to level_02 to level_02.

---
+++ Prototype Vitaliy/RescueValhalla_test/Assets/_Scripts/EndGoal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGoal : MonoBehaviour {
+    [SerializeField] bool ragnarReady = false;
+    [SerializeField] bool rokReady = false;
+    
+	void OnTriggerStay (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ragnarReady = true;
+        }
+
+        if (other.CompareTag("Dwane"))
+        {
+            rokReady = true;
+        }
+    }
+        
+    void OnTriggerExit (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ragnarReady = false;
+        }
+
+        if (other.CompareTag("Dwane"))
+        {
+            rokReady = false;
+        }
+    }
+
+    void Update()
+    {
+        if (ragnarReady && rokReady)
+        {
+            NextLevel();
+        }
+    
+    }
+
+    void NextLevel()
+    {
+        GetComponent<SceneTransition>().NextLevelButton("Level_02");
+    }
+}
--- Prototype Vitaliy/RescueValhalla_test/Assets/_Scripts/UIScripts/SceneTransition.cs
+++ Prototype Vitaliy/RescueValhalla_test/Assets/_Scripts/UIScripts/SceneTransition.cs
@@ -1,19 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class SceneTransition : MonoBehaviour {
 
-
-
-
 	//Attach this script to any button and just type in the scene name as a string.
 	public void NextLevelButton(string LevelName)
-	{
-		
-		Application.LoadLevel (LevelName);
+	{		
+		SceneManager.LoadScene(LevelName);
 	}
-
-
 }
