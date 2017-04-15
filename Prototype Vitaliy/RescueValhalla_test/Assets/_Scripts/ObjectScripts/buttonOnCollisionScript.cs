799118cff991e9b0842fb2de34723a3ec1ec3a59
IAN AT HOME <Iannorton01@gmail.com>
2017-04-15 15:58:04 -0400

    Changed all the levels. Still in progress

--- Prototype Vitaliy/RescueValhalla_test/Assets/_Scripts/ObjectScripts/buttonOnCollisionScript.cs
+++ Prototype Vitaliy/RescueValhalla_test/Assets/_Scripts/ObjectScripts/buttonOnCollisionScript.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class buttonOnCollisionScript : MonoBehaviour {
+    [Header("External Refrence")]
+    [SerializeField] GameObject movingPlatform;
 
 	// Use this for initialization
 	void Start () {
@@ -14,8 +16,12 @@ public class buttonOnCollisionScript : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
-		if (coll.gameObject.tag == "WoodenObject")
-			Destroy(GameObject.FindGameObjectWithTag ("Door"));
+		if ((coll.gameObject.tag == "WoodenObject"|| coll.gameObject.CompareTag("Dwane")) && movingPlatform !=null)
+        {
+            movingPlatform.GetComponent<MovingPlatform>().moveSpeed = 1.5f;
+        }
+			//Destroy(GameObject.FindGameObjectWithTag ("Door"));
+
 
 	}
 }
