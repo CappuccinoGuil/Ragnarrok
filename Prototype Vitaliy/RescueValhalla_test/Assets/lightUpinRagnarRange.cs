48e4d72c2e3a8ec39c38aa90f25a193678b112a1
vstarush <vstarush@gmail.com>
2017-04-19 23:15:42 -0400

    Added Ragnar's stone

---
+++ Prototype Vitaliy/RescueValhalla_test/Assets/lightUpinRagnarRange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightUpinRagnarRange : MonoBehaviour {
+	[SerializeField] float m_effectRadius = 2;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		LightUpPlatformsInRange ();
+	}
+
+	//Hallo effect
+	void LightUpPlatformsInRange(){
+		Collider2D[] inRange = Physics2D.OverlapCircleAll(transform.position, m_effectRadius);
+		//print (inRange.Length);
+		if (inRange.Length > 0) {
+			foreach (Collider2D item in inRange) {
+				//print (hello);
+
+				if (item.GetComponent<Rigidbody2D> () && item.CompareTag ("Ragnar")) {
+					transform.GetComponent<SpriteRenderer> ().enabled = true;
+					break;
+				} else {
+					transform.GetComponent<SpriteRenderer> ().enabled = false;
+				}
+
+			}
+		}
+	}
+}
